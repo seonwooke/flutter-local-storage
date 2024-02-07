5bd5da16ebc143b9e298af047e1005906f4e16b6
seonwooke <seonookkim@gmail.com>
2024-02-07 19:07:14 +0900

    :art: Sample View
    Sample View

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'sample_view.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+Future<void> main() async {
+  runApp(ExampleView());
 }
